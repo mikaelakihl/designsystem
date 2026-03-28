0afed761c3084bc1bfc730488913257a177ab370
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-03-28 16:19:44 +0100

    feat: create first layout and created anchorlinks as navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
- 
-
+  const anchorlinks = [{ id: 'colors', label: 'Color' }]
   return (
     <>
-      
+      <header>
+        <h1>Design System</h1>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.sidebar}>
+          <nav>
+            <ul>
+              {anchorlinks.map((anchorlink) => (
+                <li key={anchorlink.id}>
+                  <a href={`#${anchorlink.id}`}>{anchorlink.label}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.content}>
+          <section>
+            <h2 id="colors">Color</h2>
+          </section>
+        </div>
+      </main>
     </>
   )
 }
